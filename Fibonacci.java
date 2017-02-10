0f5a45be47c57f016ab95da83a3fc104a9efc4e4
Don Blair <donblair10@gmail.com>
2017-02-10 15:27:43 -0500

    Add recursive method.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList; 
+
 /** 
- * The Fibonacci class is for working with the Fibonacci series.  This version doesn't use recursion. 
+ * The Fibonacci class is for working with the Fibonacci series.  
  */  
 public class Fibonacci {
 
@@ -7,10 +9,10 @@ public class Fibonacci {
 	public static final int MAX_VALS = 100; 
 
 	/** 
-	 * Get the Fibonacci series.  
+	 * Get the Fibonacci series using iterative algorithm.  
 	 * @param numVals - number of values, must be between 1..MAX_VALS.
 	 * @throws Exception
-	 * @return integer array containing the Fibonacci series, with the 0th element having value 1. 
+	 * @return fixed-length integer array containing the Fibonacci series, with the 0th element having value 1. 
 	 */
 	public static int[] getSeries(int numVals) throws Exception {
 		if (numVals <= 0 || numVals > MAX_VALS) {
@@ -26,17 +28,56 @@ public class Fibonacci {
 		}
 		return tmpFib; 
 	}
+
+	/** 
+	 * Get the Fibonacci series using recursion. 
+	 * @param numVals - number of values, must be between 1..MAX_VALS.
+	 * @throws Exception
+	 * @return dynamic array containing the Fibonacci series, with the 0th element having value 1. 
+	 */
+	public static ArrayList<Integer> getSeriesRecursive(int numVals) throws Exception {
+		if (numVals <= 0 || numVals > MAX_VALS) {
+			throw new Exception("Illegal numVals argument in constructor for Fibonacci:  " + numVals);
+		}
+		if (numVals >= 3) {
+			ArrayList<Integer> series = getSeriesRecursive(numVals - 1);
+			series.add(series.get(numVals-2) + series.get(numVals-3));
+			return series; 
+		} else if (numVals == 2) {
+			ArrayList<Integer> series = new ArrayList<Integer>();
+			series.add(1);
+			series.add(2);
+			return series; 
+		} else  { // (numVals == 1) 
+			ArrayList<Integer> series = new ArrayList<Integer>();
+			series.add(1);
+			return series;
+		}
+	}
 }
 
 class Tester {	
 	/** 
-	 * The main routine is simply a test harness for Fibonacci
+	 * The is simply a test harness for Fibonacci
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// First compute the series recursively. 
 		try {
-			int[] fSeries = Fibonacci.getSeries(12);
-			System.out.println("Here's the Fibonacci series for " + fSeries.length + " numbers:  ");
+			ArrayList<Integer> fSeries = Fibonacci.getSeriesRecursive(8);
+			System.out.println("Here's the Fibonacci series computed recursively for " + fSeries.size() + " numbers:  ");
+			for (Integer j : fSeries) {
+				System.out.println(j);
+			}
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return;
+		}
+
+		// Next compute the series iteratively. 
+		try {
+			int[] fSeries = Fibonacci.getSeries(8);
+			System.out.println("Here's the Fibonacci series computed iteratively for " + fSeries.length + " numbers:  ");
 			for (int i : fSeries) {
 				System.out.println(i);
 			}
@@ -44,5 +85,6 @@ class Tester {
 			System.out.println(e.toString());
 			return;
 		}
+
 	}
 }  
